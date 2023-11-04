02ec83285d359e2593d4a9884be126e894f17b47
VasiliIgnatenko <30416299+VasiliIgnatenko@users.noreply.github.com>
2023-11-04 19:29:43 +0300

    Update corridor_2_room.js

--- src/scenes/rooms/corridor_2_room.js
+++ src/scenes/rooms/corridor_2_room.js
@@ -9,6 +9,25 @@ const dialogs = [
     { name: '...', text: 'Джон уверенно подходит к выходу из спальни и аккуратно приоткрывает дверь.\nВидны тени, гуляющие по дому.', sound: null },
 ]
 
+const dialogs_next_corridor = [
+    { name: '...', text: 'Джон аккуратно вышел из спальни и попали в коридор.', sound: null },
+    { name: '...', text: 'Голоса неизвестных стали ближе.', sound: null },
+    { name: '...', text: 'Нужно срочно что-то делать!', sound: null },
+]
+
+const dialogs_hide_corridor = [
+    { name: '...', text: 'Джон неуклюже забежал за статую и отставший от группы неизвестный его заметил:', sound: null },
+    { name: 'Неизвестный', text: 'Погодите-ка... Ребят, тут что-то есть', sound: null },
+    { name: '...', text: 'На его зов неспеша приходят остальные.', sound: null },
+    { name: 'Другой неизвестный', text: 'Если это очередной трофей "за лучший экранизированный роман", то я посажу тебя на него', sound: null },
+    { name: 'Неизвестный', text: 'Нет, тебе точно понравится! Ку-ку, малыш, испугался?', sound: null },
+    { name: '...', text: 'Писатель медленно выходит из-за статуи, его слепит свет фонарика.', sound: null },
+    { name: 'Джон', text: 'Наверное... А вы кто?', sound: null },
+    { name: 'Неизвестные', text: 'Сейчас, говнюк, узнаешь!"', sound: null },
+    { name: '...', text: 'Джон оглушен. Его тело нашли утром.', sound: null },
+    { name: '', text: '', sound: null, effect: 'fadeOut' },
+];
+
 export class Corridor2Scene extends Phaser.Scene {
     constructor() {
         super({key: 'corridor_2'});
@@ -52,6 +71,45 @@ export class Corridor2Scene extends Phaser.Scene {
 
                 // Пройти дальше по коридору
                 me.roomContainer.add(me.btn_3 = new Button(me, 0, 300, 'button_menu', null, () => {
+
+                    me.btn_1.visible = false;
+                    me.btn_2.visible = false;
+                    me.btn_3.visible = false;
+
+                    me.mng_dialogs.destroy();
+                    me.roomContainer.add(me.mng_dialogs = new Dialogs_Manager(me,-400, 0, dialogs_next_corridor.reverse()));
+
+                    me.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE, true);
+                    me.input.keyboard.on('keydown-SPACE', () => {
+                        if (me.mng_dialogs.isNextDialog()) {
+                            me.mng_dialogs.nextDialog();
+                            me.mng_dialogs.hidePrevDialogs();
+                        } else {
+
+                            me.roomContainer.add(me.btn_4 = new Button(me, 0, 100, 'button_menu', null, () => {
+                                me.scene.start('children');
+                            }, me));
+                            me.btn_4.addLabel(0, 0, 'Зайти в детскую', {fontSize: '18px'});
+
+                            me.roomContainer.add(me.btn_5 = new Button(me, 0, 200, 'button_menu', null, () => {
+
+                                me.btn_4.visible = false;
+                                me.btn_5.visible = false;
+
+                                me.mng_dialogs.destroy();
+                                me.roomContainer.add(me.mng_dialogs = new Dialogs_Manager(me,-400, 0, dialogs_hide_corridor.reverse()));
+
+                                me.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE, true);
+                                me.input.keyboard.on('keydown-SPACE', () => {
+                                    if (me.mng_dialogs.isNextDialog()) {
+                                        me.mng_dialogs.nextDialog();
+                                        me.mng_dialogs.hidePrevDialogs();
+                                    }
+                                });
+                            }, me));
+                            me.btn_5.addLabel(0, 0, 'Постараться пройти дальше\nи спрятаться за углом/скульптурой', {fontSize: '18px'});
+                        }
+                    });
 
                 }, me));
                 me.btn_3.addLabel(0, 0, 'Пройти дальше по коридору', {fontSize: '18px'});
