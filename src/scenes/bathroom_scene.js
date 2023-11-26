375e581592e9a33dbee603c6a3f9dd25d660675b
VasiliIgnatenko <30416299+VasiliIgnatenko@users.noreply.github.com>
2023-11-26 17:35:47 +0300

    add person

--- src/scenes/bathroom_scene.js
+++ src/scenes/bathroom_scene.js
@@ -5,33 +5,33 @@ import {Dialogs_Manager} from "../core/dialogs_manager";
 import {unitmng} from "../game";
 
 const dialogs_part_1 = [
-    { name: '...', text: 'Зайдя в ванную Джон тихо прикрыл за собой дверь и подошел к зеркалу.', sound: null },
-    { name: 'Джон', text: 'До чего я докатился... но все еще хорош', sound: null },
-    { name: '...', text: 'Писатель быстро осматривает себя, проверяет дыхание', sound: null },
-    { name: 'Джон', text: 'Фу, сука, будто кошка сдохла!', sound: null },
-    { name: '...', text: 'Медленно поворачивает кран, слегка умывает себя водой. Вроде становится легче…', sound: null },
-    { name: 'Неизвестный', text: 'Зацени!!!', sound: 'snd_play_music' },
-    { name: 'Неизвестный', text: 'Упс', sound: null },
-    { name: 'Джон', text: 'КОНЧЕНЫЕ, ЭТО БЫЛА СИСТЕМА ЗА 10 ШТУК,\nНАДЕЮСЬ У ВАС ЕСТЬ БАБКИ, Я ОТСУЖУ ВСЁ!!!', sound: null },
-    { name: '...', text: 'Джон зло осматривает комнату.', sound: null },
+    { image: 'person_john',    name: '...', text: 'Зайдя в ванную Джон тихо прикрыл за собой дверь и подошел к зеркалу.', sound: null },
+    { image: 'person_john',    name: 'Джон', text: 'До чего я докатился... но все еще хорош', sound: null },
+    { image: 'person_john',    name: '...', text: 'Писатель быстро осматривает себя, проверяет дыхание', sound: null },
+    { image: 'person_john',    name: 'Джон', text: 'Фу, сука, будто кошка сдохла!', sound: null },
+    { image: 'person_john',    name: '...', text: 'Медленно поворачивает кран, слегка умывает себя водой. Вроде становится легче…', sound: null },
+    { image: 'person_enemy_1', name: 'Неизвестный', text: 'Зацени!!!', sound: 'snd_play_music' },
+    { image: 'person_enemy_1', name: 'Неизвестный', text: 'Упс', sound: null },
+    { image: 'person_john',    name: 'Джон', text: 'КОНЧЕНЫЕ, ЭТО БЫЛА СИСТЕМА ЗА 10 ШТУК,\nНАДЕЮСЬ У ВАС ЕСТЬ БАБКИ, Я ОТСУЖУ ВСЁ!!!', sound: null },
+    { image: 'person_john',    name: '...', text: 'Джон зло осматривает комнату.', sound: null },
 ]
 
 const dialogs_part_2 = [
-    { name: '...', text: 'Джон слышит голоса из соседней комнаты.', sound: null },
-    { name: 'Джон', text: 'Они близко, надо уходить.', sound: null },
-    { name: 'Неизвестный', text: 'Ты везде посмотрел? Где он?', sound: null },
-    { name: 'Другой неизвестный', text: 'Да осталась всего пара комнат...', sound: null },
-    { name: 'Неизвестный', text: 'В смысле всего пара?! Билл сказал что ты все обыскал!', sound: null },
-    { name: 'Другой неизвестный', text: 'Тише-тише... Пупсик, он здесь, я точно знаю. Мы найдем его и ты сделаешь все, что пожелаешь. \nЛучше угомони девку, потом решим что с ней сделаем. Пара идей у меня точно есть', sound: null },
-    { name: '...', text: 'Слышны всхлипы и женский голос.', sound: 'snd_girl_voice' },
-    { name: 'Девушка', text: 'Да что вам нужно?! Отпу...', sound: 'snd_girl_voice' },
-    { name: '...', text: 'Слышны звуки борьбы. Вероятнее всего девушке завязали рот.', sound: 'snd_girl_voice' },
-    { name: 'Джон', text: 'Странно, голос очень знаком. Нужно двинуться дальше. Может я смогу ей помочь...', sound: null },
-    { name: '...', text: 'Джон подходит к двери, проходит через кабинет, и быстро пробирается в коридор.', sound: null },
+    { image: 'person_john',    name: '...', text: 'Джон слышит голоса из соседней комнаты.', sound: null },
+    { image: 'person_john',    name: 'Джон', text: 'Они близко, надо уходить.', sound: null },
+    { image: 'person_enemy_1', name: 'Неизвестный', text: 'Ты везде посмотрел? Где он?', sound: null },
+    { image: 'person_enemy_2', name: 'Другой неизвестный', text: 'Да осталась всего пара комнат...', sound: null },
+    { image: 'person_enemy_1', name: 'Неизвестный', text: 'В смысле всего пара?! Билл сказал что ты все обыскал!', sound: null },
+    { image: 'person_enemy_2', name: 'Другой неизвестный', text: 'Тише-тише... Пупсик, он здесь, я точно знаю. Мы найдем его и ты сделаешь все, что пожелаешь. \nЛучше угомони девку, потом решим что с ней сделаем. Пара идей у меня точно есть', sound: null },
+    { image: 'person_meredit', name: '...', text: 'Слышны всхлипы и женский голос.', sound: 'snd_girl_voice' },
+    { image: 'person_meredit', name: 'Девушка', text: 'Да что вам нужно?! Отпу...', sound: 'snd_girl_voice' },
+    { image: 'person_meredit', name: '...', text: 'Слышны звуки борьбы. Вероятнее всего девушке завязали рот.', sound: 'snd_girl_voice' },
+    { image: 'person_john',    name: 'Джон', text: 'Странно, голос очень знаком. Нужно двинуться дальше. Может я смогу ей помочь...', sound: null },
+    { image: 'person_john',    name: '...', text: 'Джон подходит к двери, проходит через кабинет, и быстро пробирается в коридор.', sound: null },
 ]
 
 const dialogs_attempt = [
-    { name: 'Джон', text: 'Ничего...', sound: null },
+    { image: 'person_john', name: 'Джон', text: 'Ничего...', sound: null },
 ]
 
 export class BathroomScene extends Phaser.Scene {
